1c0d6836485325dc73d1a96aaa7cb80e8a662bac
vinayak200421 <186008320+vinayak200421@users.noreply.github.com>
2026-02-02 21:54:59 +0530

    UtilCode added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port: ${procss.env.PORT}`);
+    })
+})
+.catch((error)=>{
+console.log("MONGO db connection failed !!! ",error);
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(stusCode, data,message="Success"){
+        this.statusCode=this.statusCode
+        this.data=data
+        this.message=message
+        this.sucess=statusCode<400
+    }
+}
